82b9c53eb4bb11a712d78ef6bed097bbf22ba32d
Kevin Sheng <kevinsheng2005@gmail.com>
2026-03-14 21:35:20 -0700

    some macro/script stuff

---
+++ content/math/mathjax.js
@@ -0,0 +1,12 @@
+window.MathJax = {
+    tex: {
+        inlineMath: {'[+]': [['$', '$']]},
+        macros: {
+            N: "\\mathbb{N}",
+            R: "\\mathbb{R}",
+            Q: "\\mathbb{Q}",
+            Z: "\\mathbb{Z}",
+            C: "\\mathbb{C}",
+        }
+    }
+};
